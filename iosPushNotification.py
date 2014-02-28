5fe435490ae4244bffd31bd0d078b5c4b8294df9
Xu Xiao Jiang <must2334@foxmail.com>
2014-02-28 10:18:00 +0800

    add support for separate key cert pen file and original p12 key file, cert file.

--- iosPushNotification.py
+++ iosPushNotification.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import os, ast, ssl, json ,socket, struct, binascii, sys, getopt
+import os, ast, ssl, json ,socket, struct, binascii, sys, getopt, random, string
 from OpenSSL.crypto import *
 
 def main(argv):
@@ -17,7 +17,7 @@ def main(argv):
 
     #parsering the arguments
     try:
-         opts, args = getopt.getopt(argv,"hk:c:p:m:t:d:",["help", "keyFile=", "cerFile=", "KeyPemFile=", "cerPemFile=", "pemFile=", "token=", "payload=", "development="])
+         opts, args = getopt.getopt(argv,"hk:c:p:m:t:d:",["help", "keyFile=", "cerFile=", "keyPemFile=", "cerPemFile=", "pemFile=", "token=", "payload=", "development="])
     except getopt.GetoptError:
         print 'please use iosPushNotification.py -h or iosPushNotification.py --help for help'
         sys.exit(2)
@@ -25,10 +25,10 @@ def main(argv):
         if opt in ("-h", "--help"):
             print """
 #######################--help--#######################
-    -k --keyFile : for the p12 file. (no supported yet)
-    -c --cerFile : for the cert file. (no supported yet)
-    --keyPemFile : for pem file that generated from the p12 file. (no supported yet)
-    --cerPemFile : for pem file that generated from the cert file. (no supported yet)
+    -k --keyFile : for the p12 file.
+    -c --cerFile : for the cert file.
+    --keyPemFile : for pem file that generated from the p12 file.
+    --cerPemFile : for pem file that generated from the cert file.
     --pemFile : the combined pem file, which is most widely used.
     -t --token : the token of device to receive this notification.
     -m --payload : the payload to be sent.
@@ -67,17 +67,48 @@ def main(argv):
         send_push_notification(token, payload, pemFile, dev = development == 1)
     elif keyPemFile and cerPemFile :
         pemFile = create_pem_file(keyPemFile, cerPemFile)
+        send_push_notification(token, payload, pemFile, dev = development == 1)
+        os.remvoe(pemFile)
+    elif keyFile and cerFile :
+        pemFile = create_pem_file(keyFile, cerFile)
+        send_push_notification(token, payload, pemFile, dev = development == 1)
+        os.remove(pemFile)
+
 
 
 def create_pem_file(key, cert):
     if key.endswith(".p12") and cert.endswith(".cer") :
-        print "original files"
+        keyFile = open(key, "r")
+        cerFile = open(cert, "r")
+        pemFilePath = "iosPushNotification.temp" + random_string(6)
+        while (os.path.isfile(pemFilePath + ".pem")):
+            pemFilePath = "iosPushNotification.temp" + random_string(6)
+        pemFilePath = pemFilePath + ".pem"
+        pemFile = open(pemFilePath, "w")
+        p12 = load_pkcs12(keyFile.read(), "")
+        x509 = load_certificate(FILETYPE_ASN1, cerFile.read())
+        pemFile.write(dump_certificate(FILETYPE_PEM,x509))
+        pemFile.write(dump_privatekey(FILETYPE_PEM,p12.get_privatekey()))
+        pemFile.close()
+        return pemFilePath
     elif key.endswith(".pem") and cert.endswith(".pem") :
-        print "pem files"
+        keyPemFile = open(key, "r")
+        cerPemFile = open(cert, "r")
+        pemFilePath = "iosPushNotification.temp" + random_string(6)
+        while (os.path.isfile(pemFilePath + ".pem")):
+            pemFilePath = "iosPushNotification.temp" + random_string(6)
+        pemFilePath = pemFilePath + ".pem"
+        pemFile = open(pemFilePath, "w")
+        pemFile.write(keyPemFile.read())
+        pemFile.write(cerPemFile.read())
+        pemFile.close()
+        return pemFilePath
     else:
         print "plese using correct key file and cert file."
         return ""
 
+def random_string(length):
+    return "".join(random.choice(string.ascii_uppercase + string.digits) for x in range(length))
 
 def send_push_notification(token, payload, cert = "", dev = True):     
     # the certificate file generated from Provisioning Portal     
